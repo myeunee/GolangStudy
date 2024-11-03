a59d31e27cc5ae67e4f3bb9d9c1b3907c7b0bc30
myeunee <myeunee@gmail.com>
2024-11-03 20:25:23 +0900

    도커파일 버전 최신으로 수정

---
+++ chapter16/section61/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/myeunee/GolangStudy/chapter16/section61/config"
+	"golang.org/x/sync/errgroup"
+)
+
+// run 함수만 호출하도록
+func main() {
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminated server: %v", err)
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context) error {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
+	}
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
+	s := &http.Server{
+		// 인수로 받은 net.Listener를 이용 -> Addr 필드 지정 X
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// 명령줄에서 테스트하기 위한 로직
+			time.Sleep(5 * time.Second)
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	}
+	eg, ctx := errgroup.WithContext(ctx)
+	// 다른 고루틴에서 http 서버 실행
+	eg.Go(func() error {
+		// ListenAndServe 가 아니라 Serve 메서드로 변경
+		if err := s.Serve(l); err != nil &&
+			// http.ErrServerClosed는
+			// http.Server.Shutdown()가 정상 종료되었다고 표시하므로 문제 X。
+			err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	// 채널로부터 알림(종료 알림)을 기다림
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+	// Go메서드로 실행한 다른 고루틴의 종료를 기다림
+	return eg.Wait()
+}
