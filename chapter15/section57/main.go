4a6b851f43de4654f141e27d62f3473713479b93
myeunee <myeunee@gmail.com>
2024-11-04 00:03:20 +0900

    .air.toml 파일명 수정

--- chapter15/section57/main.go
+++ chapter15/section57/main.go
@@ -4,21 +4,37 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 
 	"golang.org/x/sync/errgroup"
 )
 
-/******** section 55 main.go 참고 *******/
 func main() {
-	if err := run(context.Background()); err != nil {
-		log.Printf("failed to terminate server: %v", err)
+	if len(os.Args) != 2 {
+		log.Printf("need port number\n")
+		os.Exit(1)
+	}
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %v", p, err)
+	}
+	// l.Addr() 메서드로 네트워크 연결의 주소를 가져옴
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+
+	// 주소를 로그로 출력
+	log.Printf("start with: %v", url)
+
+	if err := run(context.Background(), l); err != nil {
+		log.Printf("failed to terminated server: %v", err)
+		os.Exit(1)
 	}
 }
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	s := &http.Server{
-		Addr: ":80", // 포트를 코드 내부에 하드 코딩
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
@@ -26,9 +42,9 @@ func run(ctx context.Context) error {
 	eg, ctx := errgroup.WithContext(ctx)
 	// 다른 고루틴에서 http 서버 실행
 	eg.Go(func() error {
-		// http.ErrServerClosed는
-		// http.Server.Shutdown() 가 정상 종료된 것을 나타냄 -> 이상 처리가 아님
-		if err := s.ListenAndServe(); err != nil &&
+		if err := s.Serve(l); err != nil &&
+			// http.ErrServerClosed는
+			// http.Server.Shutdown() 가 정상 종료된 것을 나타냄 -> 이상 처리가 아님
 			err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
 			return err
