5bd6be06cfc04dfc6404c1e655653d7ff3e128b7
myeunee <myeunee@gmail.com>
2024-11-12 19:11:27 +0900

    [1112] section68

---
+++ chapter17/section68/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/myeunee/GolangStudy/chapter17/section68/config"
+)
+
+func main() {
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminated server: %v", err)
+		os.Exit(1)
+	}
+}
+
+// 리팩토링한 run 함수
+func run(ctx context.Context) error {
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
+	}
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
+}
