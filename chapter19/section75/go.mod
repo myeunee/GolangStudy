4f3148b813f87f44165e87c1c7f45ce2eea2cc63
myeunee <myeunee@gmail.com>
2024-11-25 04:42:32 +0900

    [1125] section75

--- chapter19/section75/go.mod
+++ chapter19/section75/go.mod
@@ -7,12 +7,15 @@ require (
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/go-playground/validator/v10 v10.22.1
 	github.com/google/go-cmp v0.6.0
-	golang.org/x/sync v0.8.0
+	github.com/matryer/moq v0.5.0
+	golang.org/x/sync v0.9.0
 )
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
+	golang.org/x/mod v0.22.0 // indirect
+	golang.org/x/tools v0.27.0 // indirect
 )
 
 require (
@@ -23,8 +26,8 @@ require (
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/leodido/go-urn v1.4.0 // indirect
-	golang.org/x/crypto v0.19.0 // indirect
-	golang.org/x/net v0.21.0 // indirect
-	golang.org/x/sys v0.23.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/crypto v0.29.0 // indirect
+	golang.org/x/net v0.31.0 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
 )
--- chapter19/section75/mux.go
+++ chapter19/section75/mux.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 
 	"github.com/myeunee/GolangStudy/chapter19/section75/clock"
 	"github.com/myeunee/GolangStudy/chapter19/section75/config"
 	"github.com/myeunee/GolangStudy/chapter19/section75/handler"
+	"github.com/myeunee/GolangStudy/chapter19/section75/service"
 	"github.com/myeunee/GolangStudy/chapter19/section75/store"
 
 	"github.com/go-chi/chi/v5"
@@ -16,22 +16,24 @@ import (
 
 func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), error) {
 	mux := chi.NewRouter()
-	// chi.Router의 HandlerFunc은 라우팅 패턴과, 핸들러 함수를 받음
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		_, _ = w.Write([]byte(`{"status": "ok"}`))
-
 	})
-	fmt.Print("핸들러 등록 성공")
 	v := validator.New()
-	db, cleanup, err := store.New(ctx, cfg) // connection 생성
+	db, cleanup, err := store.New(ctx, cfg)
 	if err != nil {
 		return nil, cleanup, err
 	}
 	r := store.Repository{Clocker: clock.RealClocker{}}
-	at := &handler.AddTask{DB: db, Repo: &r, Validator: v} // AddTask를 handler로 사용
+	at := &handler.AddTask{
+		Service:   &service.AddTask{DB: db, Repo: &r},
+		Validator: v,
+	}
 	mux.Post("/tasks", at.ServeHTTP)
-	lt := &handler.ListTask{DB: db, Repo: &r}
+	lt := &handler.ListTask{
+		Service: &service.ListTask{DB: db, Repo: &r},
+	}
 	mux.Get("/tasks", lt.ServeHTTP)
 	return mux, cleanup, nil
 }
