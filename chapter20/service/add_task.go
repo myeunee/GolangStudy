b11b5ec9b80dec1645f045f5b559073e012b7d16
myeunee <myeunee@gmail.com>
2024-11-26 18:41:39 +0900

    [1126] section 82

---
+++ chapter20/service/add_task.go
@@ -0,0 +1,26 @@
+package service
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/myeunee/GolangStudy/chapter20/entity"
+	"github.com/myeunee/GolangStudy/chapter20/store"
+)
+
+type AddTask struct {
+	DB   store.Execer
+	Repo TaskAdder
+}
+
+func (a *AddTask) AddTask(ctx context.Context, title string) (*entity.Task, error) {
+	t := &entity.Task{
+		Title:  title,
+		Status: entity.TaskStatusTodo,
+	}
+	err := a.Repo.AddTask(ctx, a.DB, t)
+	if err != nil {
+		return nil, fmt.Errorf("failed to register: %w", err)
+	}
+	return t, nil
+}
---
+++ chapter20/service/interface.go
@@ -0,0 +1,20 @@
+package service
+
+import (
+	"context"
+
+	"github.com/myeunee/GolangStudy/chapter20/entity"
+	"github.com/myeunee/GolangStudy/chapter20/store"
+)
+
+//go:generate go run github.com/matryer/moq -out moq_test.go . TaskAdder TaskLister UserRegister
+type TaskAdder interface {
+	AddTask(ctx context.Context, db store.Execer, t *entity.Task) error
+}
+type TaskLister interface {
+	ListTasks(ctx context.Context, db store.Queryer) (entity.Tasks, error)
+}
+
+type UserRegister interface {
+	RegisterUser(ctx context.Context, db store.Execer, user *entity.User) error
+}
